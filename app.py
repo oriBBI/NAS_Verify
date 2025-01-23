9d9b87fc2752f2bc143509e24ad76d63920cc8e6
oribbi <hd6477@gmail.com>
2025-01-24 00:11:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -76,28 +76,28 @@ def main():
                         col1, col2, col3, col4 = st.columns(4)  # 한 행에 4개의 열로 나누기
                         with col1:
                             
-                            fig, ax = plt.subplots(figsize=(6,4))
+                            fig, ax = plt.subplots(figsize=(15,10))
                             stock.PBR_graph(True, ax=ax)  # PBR 그래프 그리기
                             st.pyplot(fig)  # Streamlit에 그래프 표시
                             st.write('PBR_graph')
                         
                         with col2:
                             
-                            fig, ax = plt.subplots(figsize=(6,4))
+                            fig, ax = plt.subplots(figsize=(15,10))
                             stock.ROE_graph(True, ax=ax)  # ROE 그래프 그리기
                             st.pyplot(fig)  # Streamlit에 그래프 표시
                             st.write('ROE_graph')
                         
                         with col3:
                             
-                            fig, ax = plt.subplots(figsize=(6,4))
+                            fig, ax = plt.subplots(figsize=(15,10))
                             stock.Revenue_Net_income_graph(True, ax=ax)  # Revenue & Net Income 그래프 그리기
                             st.pyplot(fig)  # Streamlit에 그래프 표시
                             st.write('Revenue_Net_income_graph')
                         
                         with col4:
                             
-                            fig, ax = plt.subplots(figsize=(6,4))
+                            fig, ax = plt.subplots(figsize=(15,10))
                             stock.price_52_graph(True, ax=ax)  # Revenue & Net Income 그래프 그리기
                             st.pyplot(fig)  # Streamlit에 그래프 표시
                             st.write('price_52_graph')
@@ -111,8 +111,7 @@ def main():
                 trash = pd.concat([trash, trash_ticker], axis=0, ignore_index=False)
                 trash.to_csv('trash.csv')
             #print(f'{ticker}검증 끝')
-            
-        
+      
 if __name__ == '__main__':
     main()
 
